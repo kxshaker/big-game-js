3904929ecc34d37bd3ff5f2cb8f669f3715982c8
r00t-1n5ta113r <57874692+r00t-in5ta11er@users.noreply.github.com>
2020-10-25 08:18:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game.
 */
+
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
